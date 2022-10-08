51330a51fe33e000db7c9468cd4732c13c09dfe1
Ishita Sharma <95963916+IshitaSharma-0651@users.noreply.github.com>
2022-10-08 05:58:44 +0530

    Time: 3 ms (72.03%), Space: 42 MB (48.71%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -5,11 +5,11 @@ class Solution {
        map.put(']','[');
         map.put(')','(');
         map.put('}','{');
-        Stack <Character> st=new Stack<>();
+      /*  Stack <Character> st=new Stack<>();
         for(int i=0;i<s.length();i++)
         {
             char c=s.charAt(i);
-            if(map.containsValue(c))
+            if(map.containsValue(c))//to ensure that it is a opening bracket
                 st.push(c);
             else
             {
@@ -19,5 +19,37 @@ class Solution {
             }
         }
         return st.isEmpty();
+        */
+        int top=-1;
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if(map.containsValue(c))
+                top=i;
+            else
+            {
+                if(top==-1 || map.get(c)!=s.charAt(top))
+                    return false;
+                else 
+                    top=getTop(s,top-1);
+            }
+        }
+        return top==-1;
+    }
+    public int getTop(String s, int index)
+    {
+       int right=0;
+        while(index>=0)
+        {
+            char ch=s.charAt(index);
+            if(map.containsValue(ch))
+                right--;
+            else 
+                right++;
+            if(right<0)
+                return index;
+            index--;
+        }
+        return -1;
     }
 }
